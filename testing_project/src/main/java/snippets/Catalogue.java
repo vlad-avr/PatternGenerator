f1d4c99ad89039d7a045e390a38eb9489aa3bdb8
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2024-01-08 17:17:57 +0200

    reverted back to stable version

--- testing_project/src/main/java/snippets/Catalogue.java
+++ testing_project/src/main/java/snippets/Catalogue.java
@@ -4,9 +4,5 @@ public class Catalogue{
 	//Options
 	 public final static String TestTemplate = "TestTemplate";
 	 public final static String AnotherTestTemplate = "AnotherTestTemplate";
-	 public final static String AnotherTemplate = "AnotherTemplate";
-	 public final static String CustomTemplate = "CustomTemplate";
 }
-
-
 
